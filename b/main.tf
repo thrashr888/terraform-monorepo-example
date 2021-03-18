cc0e869fee17da838b310007012d31d24e61d6a1
Paul Thrasher <pthrasher@hashicorp.com>
2021-03-18 12:48:23 -0700

    Create main.tf

---
+++ b/main.tf
@@ -0,0 +1,15 @@
+resource "random_id" "random" {
+  keepers = {
+    uuid = uuid()
+  }
+  
+  byte_length = 8
+}
+
+output "random" {
+  value = random_id.random.hex
+}
+
+output "name" {
+  value = "b"
+}
